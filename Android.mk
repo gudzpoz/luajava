16dd7e9eaac758542e6fb6921933433dcc312ed6
Thomas Slusny <slusnucky@gmail.com>
2015-05-11 17:20:47 +0200

    Added lua 5.3

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
  
-LOCAL_MODULE    := gdx
+LOCAL_MODULE    := non
 LOCAL_C_INCLUDES := 
  
 LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O2 -Wall -D__ANDROID__
@@ -11,5 +11,7 @@ LOCAL_ARM_MODE  := arm
  
 LOCAL_SRC_FILES := luajava.c\
 	memcpy_wrap.c
+  
+LOCAL_SRC_FILES := luajava.c memcpy_wrap.c lua/lapi.c lua/lauxlib.c lua/lbaselib.c lua/lcode.c lua/ldblib.c lua/ldebug.c lua/ldo.c lua/ldump.c lua/lfunc.c lua/lgc.c lua/linit.c lua/liolib.c lua/llex.c lua/lmathlib.c lua/lmem.c lua/loadlib.c lua/lobject.c lua/lopcodes.c lua/loslib.c lua/lparser.c lua/lstate.c lua/lstring.c lua/lstrlib.c lua/ltable.c lua/ltablib.c lua/ltm.c lua/lundump.c lua/lvm.c lua/lzio.c
  
 include $(BUILD_SHARED_LIBRARY)
