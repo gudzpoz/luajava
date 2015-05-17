68ee246423909b5a45152f29e31d22a3a8b5acd9
Thomas Slusny <slusnucky@gmail.com>
2015-05-17 19:42:28 +0200

    Project template is fully working, everything is awesome :smile:

--- template/desktop/src/com/mylua/project/desktop/DesktopLauncher.java
+++ template/desktop/src/com/mylua/project/desktop/DesktopLauncher.java
@@ -1,12 +1,9 @@
 package com.mylua.project.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mylua.project.MyLuaProject;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyLuaProject(), config);
-	}
+    public static void main (String[] arg) {
+        new MyLuaProject();
+    }
 }
