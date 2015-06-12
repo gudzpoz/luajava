0740d4cd08ad6ea3487e14509b35b40927305aaa
Thomas Slusny <slusnucky@gmail.com>
2015-06-12 05:54:27 +0200

    Fix indentation [ci skip]

--- template/libgdx/android/assets/main.lua
+++ template/libgdx/android/assets/main.lua
@@ -6,20 +6,20 @@ local Texture = java.require("com.badlogic.gdx.graphics.Texture")
 local batch, img
 
 function create()
-	batch = java.new(SpriteBatch)
-	img = java.new(Texture, "badlogic.jpg")
+  batch = java.new(SpriteBatch)
+  img = java.new(Texture, "badlogic.jpg")
 end
 
 function render()
-	Gdx.gl:glClearColor(1, 0, 0, 1)
-	Gdx.gl:glClear(GL20.GL_COLOR_BUFFER_BIT)
+  Gdx.gl:glClearColor(1, 0, 0, 1)
+  Gdx.gl:glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-	batch:begin()
-	batch:draw(img, 0, 0)
-	batch:end()
+  batch:begin()
+  batch:draw(img, 0, 0)
+  batch:end()
 end
 
 function dispose()
-	batch:dispose()
-	img:dispose()
+  batch:dispose()
+  img:dispose()
 end
