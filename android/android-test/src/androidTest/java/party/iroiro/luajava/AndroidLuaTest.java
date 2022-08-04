4b99481ee965b493d447f7b2aac3d12d0a338567
gudzpoz@live.com <GuDzpoz@live.com>
2022-08-04 11:41:00 +0800

    Fixed binary building config for Android

--- android/android-test/src/androidTest/java/party/iroiro/luajava/AndroidLuaTest.java
+++ android/android-test/src/androidTest/java/party/iroiro/luajava/AndroidLuaTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 public class AndroidLuaTest {
     @Test
     public void lua51Test() {
+        new LuaTestSuite<>(new Lua51(), Lua51::new).test();
     }
     @Test
     public void lua52Test() {
