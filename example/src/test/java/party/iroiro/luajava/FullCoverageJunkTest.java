8c9061e5549232ae7670ab3a5cf2043b70612d3b
gudzpoz@live.com <GuDzpoz@live.com>
2022-08-18 15:06:06 +0800

    Added some full coverage tests

--- example/src/test/java/party/iroiro/luajava/FullCoverageJunkTest.java
+++ example/src/test/java/party/iroiro/luajava/FullCoverageJunkTest.java
@@ -1,7 +1,11 @@
 package party.iroiro.luajava;
 
 import org.junit.jupiter.api.Test;
+import party.iroiro.luajava.value.ImmutableLuaValue;
+import party.iroiro.luajava.value.LuaValue;
+import party.iroiro.luajava.value.RefLuaValue;
 
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class FullCoverageJunkTest {
@@ -17,5 +21,14 @@ public class FullCoverageJunkTest {
         Lua54Consts lua54Consts = new Lua54Consts() {};
         LuaJitConsts luaJitConsts = new LuaJitConsts() {};
         assertNull(JuaAPI.CONSTRUCTOR_WRAPPER.getName(FullCoverageJunkTest.class.getConstructor()));
+
+        Lua51 L = new Lua51();
+        //noinspection deprecation
+        ImmutableLuaValue.NIL(L).close();
+        L.createTable(0, 0);
+        LuaValue v = L.get();
+        assertInstanceOf(RefLuaValue.class, v);
+        //noinspection deprecation
+        v.close();
     }
 }
