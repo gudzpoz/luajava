c0c84c011ae09f8b2d4338aecd954a6bd86817a7
gudzpoz@live.com <GuDzpoz@live.com>
2022-09-06 22:05:19 +0800

    Added basic JSR-223 provider

---
+++ jsr223/src/test/java/party/iroiro/luajava/LuaScriptEngineFactoryTest.java
@@ -0,0 +1,93 @@
+package party.iroiro.luajava;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LuaScriptEngineFactoryTest {
+    @Test
+    public void simpleTest() {
+        LuaScriptEngineFactory factory = new LuaScriptEngineFactory();
+        assertInstanceOf(LuaScriptEngine.class, factory.getScriptEngine());
+    }
+
+    @Test
+    public void preferredVersionTest() {
+        System.setProperty("luajava.jsr-223", "54");
+        new LuaScriptEngineFactory();
+        System.setProperty("luajava.jsr-223", "jit");
+        new LuaScriptEngineFactory();
+        System.setProperty("luajava.jsr-223", "nope");
+        new LuaScriptEngineFactory();
+        System.clearProperty("luajava.jsr-223");
+        new LuaScriptEngineFactory();
+    }
+
+    @Test
+    public void notFoundTest() throws NoSuchFieldException, IllegalAccessException {
+        Field engineField = LuaScriptEngineFactory.class.getDeclaredField("ENGINES");
+        engineField.setAccessible(true);
+        String[][] engines = (String[][]) engineField.get(null);
+        ArrayList<String> stash = new ArrayList<>(engines.length);
+        for (String[] engine : engines) {
+            stash.add(engine[2]);
+            engine[2] = "nonsense";
+        }
+        assertThrows(LinkageError.class, LuaScriptEngineFactory::new);
+        for (int i = 0; i < engines.length; i++) {
+            engines[i][2] = stash.get(i);
+        }
+    }
+
+    @Test
+    public void paramTest() {
+        LuaScriptEngineFactory factory = new LuaScriptEngineFactory();
+        assertEquals("Lua 5.4", factory.getEngineName());
+        assertEquals("5.4.4", factory.getEngineVersion());
+        assertIterableEquals(Collections.singletonList("lua"), factory.getExtensions());
+        List<String> mimeTypes = new ArrayList<>();
+        mimeTypes.add("text/x-lua");
+        mimeTypes.add("application/x-lua");
+        assertIterableEquals(mimeTypes, factory.getMimeTypes());
+        assertIterableEquals(Collections.singletonList("lua54"), factory.getNames());
+        assertEquals("Lua", factory.getLanguageName());
+        assertEquals("5.4.4", factory.getLanguageVersion());
+
+        String[][] params = {
+                {"ENGINE", "Lua 5.4"},
+                {"ENGINE_VERSION", "5.4.4"},
+                {"LANGUAGE", "Lua"},
+                {"LANGUAGE_VERSION", "5.4.4"},
+                {"NAME", "lua54"},
+                {"THREADING", "THREAD-ISOLATED"},
+                {"NONSENSE", null},
+        };
+        for (String[] param : params) {
+            assertEquals(param[1], factory.getParameter(param[0]));
+        }
+        //noinspection ConstantConditions
+        assertThrows(NullPointerException.class, () -> factory.getParameter(null));
+    }
+
+    @Test
+    public void concatTest() {
+        LuaScriptEngineFactory factory = new LuaScriptEngineFactory();
+        assertEquals(
+                "o:m(a, b, c)",
+                factory.getMethodCallSyntax("o", "m", "a", "b", "c")
+        );
+        assertEquals(
+                "print(1);\nprint(2);\n",
+                factory.getProgram("print(1)", "print(2)")
+        );
+        assertEquals(
+                "print(\"\\x61\")",
+                factory.getOutputStatement("a")
+        );
+    }
+}
---
+++ jsr223/src/test/java/party/iroiro/luajava/LuaScriptEngineTest.java
@@ -0,0 +1,99 @@
+package party.iroiro.luajava;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import party.iroiro.luajava.value.LuaValue;
+
+import javax.script.*;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LuaScriptEngineTest {
+    private static LuaScriptEngine lua;
+
+    @BeforeAll
+    static void init() {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        lua = (LuaScriptEngine) manager.getEngineByExtension("lua");
+    }
+
+    @Test
+    public void scriptEngineFactoryTest() {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        assertInstanceOf(LuaScriptEngine.class, manager.getEngineByExtension("lua"));
+        assertInstanceOf(LuaScriptEngine.class, manager.getEngineByName("lua54"));
+        assertInstanceOf(LuaScriptEngine.class, manager.getEngineByMimeType("text/x-lua"));
+        assertInstanceOf(LuaScriptEngine.class, manager.getEngineByExtension("lua"));
+    }
+
+    @Test
+    public void evalTest() throws ScriptException {
+        LuaValue[] values = (LuaValue[]) lua.eval("print('Hello')");
+        assertNull(values);
+        values = (LuaValue[]) lua.eval("return 1");
+        assertEquals(1, values.length);
+        assertEquals(1., values[0].toJavaObject());
+
+        values = (LuaValue[]) lua.eval(new StringReader("print('Hello')"));
+        assertNull(values);
+    }
+
+    @Test
+    public void compiledTest() throws ScriptException {
+        LuaValue[] values = (LuaValue[]) ((Compilable) lua).compile("print('Hello')").eval();
+        assertNull(values);
+        values = (LuaValue[]) ((Compilable) lua).compile("return 1").eval();
+        assertEquals(1., values[0].toJavaObject());
+        values = (LuaValue[]) ((Compilable) lua).compile(new StringReader("return 1")).eval();
+        assertEquals(1., values[0].toJavaObject());
+    }
+
+    @Test
+    public void scopeTest() throws ScriptException {
+        lua.setBindings(null, ScriptContext.GLOBAL_SCOPE);
+        lua.eval("print()");
+        lua.setBindings(new SimpleBindings(), ScriptContext.GLOBAL_SCOPE);
+        lua.getBindings(ScriptContext.GLOBAL_SCOPE).put("javaTest1", "scopeTest");
+        lua.getBindings(ScriptContext.ENGINE_SCOPE).put("javaTest2", "scopeTest");
+        assertDoesNotThrow(() -> lua.eval("print(javaTest1 .. javaTest2)"));
+    }
+
+    @Test
+    public void exceptionTest() {
+        assertThrows(ScriptException.class, () ->
+                lua.eval("print(nil .. 1)"));
+        assertThrows(NullPointerException.class, () -> lua.eval((Reader) null));
+        //noinspection NullableProblems
+        assertThrows(ScriptException.class, () -> lua.eval(new Reader() {
+            @Override
+            public int read(char[] chars, int i, int i1) {
+                return -1;
+            }
+
+            @Override
+            public void close() throws IOException {
+                throw new IOException();
+            }
+        }));
+
+        Compilable compilable = lua;
+        assertThrows(ScriptException.class, () ->
+                compilable.compile("((("));
+        assertThrows(ScriptException.class, () ->
+                compilable.compile("print(nil .. 1)").eval());
+
+        assertThrows(ScriptException.class,
+                () -> new LuaScriptEngine("nonsense", null).eval("print()"));
+    }
+
+    @Test
+    public void stubTest() {
+        assertInstanceOf(LuaScriptEngineFactory.class, lua.getFactory());
+        assertInstanceOf(SimpleScriptContext.class, lua.getContext());
+        assertInstanceOf(SimpleBindings.class, lua.createBindings());
+    }
+}
