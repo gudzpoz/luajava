04535f65208c7dd1f2076c41645978a8ea239396
gudzpoz <GuDzpoz@live.com>
2023-03-04 08:19:10 +0000

    Deploying to gh-pages from @ gudzpoz/luajava@041163fc9d678de3a999f23725c61cd3cc283d09 🚀

---
+++ assets/jpms.html.2ea77bab.js
@@ -0,0 +1,49 @@
+import{_ as u,r as c,o as p,c as i,a,b as s,w as e,e as n}from"./app.f21b84d6.js";const r={},k=a("h1",{id:"java-9-platform-module-system",tabindex:"-1"},[a("a",{class:"header-anchor",href:"#java-9-platform-module-system","aria-hidden":"true"},"#"),n(" Java 9 Platform Module System")],-1),d=n("The "),m={href:"https://en.wikipedia.org/wiki/Java_Platform_Module_System",target:"_blank",rel:"noopener noreferrer"},_=n("Java Platform Module System"),v=n(" is a built-in way to express relationships between Java "),h=a("em",null,"modules",-1),j=n(", starting from Java 9."),y=a("p",null,[n("To use LuaJava in your Java module, simply place the follow "),a("code",null,"requires"),n(" statements in your "),a("code",null,"module-info.java"),n(":")],-1),g=a("div",{class:"language-java ext-java"},[a("pre",{class:"language-java"},[a("code",null,[a("span",{class:"token keyword"},"module"),n(),a("span",{class:"token namespace"},[n("my"),a("span",{class:"token punctuation"},"."),n("java"),a("span",{class:"token punctuation"},"."),n("module")]),n(),a("span",{class:"token punctuation"},"{"),n(`
+    `),a("span",{class:"token comment"},"// ..."),n(`
+
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava")]),a("span",{class:"token punctuation"},";"),n(`
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava"),a("span",{class:"token punctuation"},".")]),a("span",{class:"token generics"},[a("span",{class:"token punctuation"},"<"),n("replace "),a("span",{class:"token keyword"},"with"),n(" your "),a("span",{class:"token class-name"},"Lua"),n(" version"),a("span",{class:"token punctuation"},">")]),a("span",{class:"token punctuation"},";"),n(`
+
+    `),a("span",{class:"token comment"},"// ..."),n(`
+`),a("span",{class:"token punctuation"},"}"),n(`
+`)])])],-1),f=a("div",{class:"language-java ext-java"},[a("pre",{class:"language-java"},[a("code",null,[a("span",{class:"token keyword"},"module"),n(),a("span",{class:"token namespace"},[n("my"),a("span",{class:"token punctuation"},"."),n("java"),a("span",{class:"token punctuation"},"."),n("module")]),n(),a("span",{class:"token punctuation"},"{"),n(`
+    `),a("span",{class:"token comment"},"// ..."),n(`
+
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava")]),a("span",{class:"token punctuation"},";"),n(`
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava"),a("span",{class:"token punctuation"},"."),n("lua51")]),a("span",{class:"token punctuation"},";"),n(`
+
+    `),a("span",{class:"token comment"},"// ..."),n(`
+`),a("span",{class:"token punctuation"},"}"),n(`
+`)])])],-1),w=a("div",{class:"language-java ext-java"},[a("pre",{class:"language-java"},[a("code",null,[a("span",{class:"token keyword"},"module"),n(),a("span",{class:"token namespace"},[n("my"),a("span",{class:"token punctuation"},"."),n("java"),a("span",{class:"token punctuation"},"."),n("module")]),n(),a("span",{class:"token punctuation"},"{"),n(`
+    `),a("span",{class:"token comment"},"// ..."),n(`
+
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava")]),a("span",{class:"token punctuation"},";"),n(`
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava"),a("span",{class:"token punctuation"},"."),n("lua52")]),a("span",{class:"token punctuation"},";"),n(`
+
+    `),a("span",{class:"token comment"},"// ..."),n(`
+`),a("span",{class:"token punctuation"},"}"),n(`
+`)])])],-1),x=a("div",{class:"language-java ext-java"},[a("pre",{class:"language-java"},[a("code",null,[a("span",{class:"token keyword"},"module"),n(),a("span",{class:"token namespace"},[n("my"),a("span",{class:"token punctuation"},"."),n("java"),a("span",{class:"token punctuation"},"."),n("module")]),n(),a("span",{class:"token punctuation"},"{"),n(`
+    `),a("span",{class:"token comment"},"// ..."),n(`
+
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava")]),a("span",{class:"token punctuation"},";"),n(`
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava"),a("span",{class:"token punctuation"},"."),n("lua53")]),a("span",{class:"token punctuation"},";"),n(`
+
+    `),a("span",{class:"token comment"},"// ..."),n(`
+`),a("span",{class:"token punctuation"},"}"),n(`
+`)])])],-1),q=a("div",{class:"language-java ext-java"},[a("pre",{class:"language-java"},[a("code",null,[a("span",{class:"token keyword"},"module"),n(),a("span",{class:"token namespace"},[n("my"),a("span",{class:"token punctuation"},"."),n("java"),a("span",{class:"token punctuation"},"."),n("module")]),n(),a("span",{class:"token punctuation"},"{"),n(`
+    `),a("span",{class:"token comment"},"// ..."),n(`
+
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava")]),a("span",{class:"token punctuation"},";"),n(`
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava"),a("span",{class:"token punctuation"},"."),n("lua54")]),a("span",{class:"token punctuation"},";"),n(`
+
+    `),a("span",{class:"token comment"},"// ..."),n(`
+`),a("span",{class:"token punctuation"},"}"),n(`
+`)])])],-1),b=a("div",{class:"language-java ext-java"},[a("pre",{class:"language-java"},[a("code",null,[a("span",{class:"token keyword"},"module"),n(),a("span",{class:"token namespace"},[n("my"),a("span",{class:"token punctuation"},"."),n("java"),a("span",{class:"token punctuation"},"."),n("module")]),n(),a("span",{class:"token punctuation"},"{"),n(`
+    `),a("span",{class:"token comment"},"// ..."),n(`
+
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava")]),a("span",{class:"token punctuation"},";"),n(`
+    `),a("span",{class:"token keyword"},"requires"),n(),a("span",{class:"token namespace"},[n("party"),a("span",{class:"token punctuation"},"."),n("iroiro"),a("span",{class:"token punctuation"},"."),n("luajava"),a("span",{class:"token punctuation"},"."),n("luajit")]),a("span",{class:"token punctuation"},";"),n(`
+
+    `),a("span",{class:"token comment"},"// ..."),n(`
+`),a("span",{class:"token punctuation"},"}"),n(`
+`)])])],-1),J=a("h2",{id:"modules",tabindex:"-1"},[a("a",{class:"header-anchor",href:"#modules","aria-hidden":"true"},"#"),n(" Modules")],-1),L=n("All the modules are either "),T={href:"https://openjdk.org/projects/jigsaw/spec/sotms/#automatic-modules",target:"_blank",rel:"noopener noreferrer"},C=n("automatic modules"),I=n(" or "),G={href:"https://openjdk.org/projects/jigsaw/spec/sotms/#unnamed-modules",target:"_blank",rel:"noopener noreferrer"},M=n("unnamed modules"),B=n("."),E=a("ol",null,[a("li",null,[n("The "),a("code",null,"party.iroiro.luajava"),n(" module is an automatic module, wrapping around the "),a("code",null,"party.iroiro.luajava:luajava"),n(" artifact.")]),a("li",null,[n("The "),a("code",null,"party.iroiro.luajava.lua*"),n(" modules are automatic ones, wrapping Java side bindings for Lua binaries.")]),a("li",null,"The native binary JARs are put into the unnamed module.")],-1);function N(P,S){const o=c("ExternalLinkIcon"),t=c("CodeGroupItem"),l=c("CodeGroup");return p(),i("div",null,[k,a("p",null,[d,a("a",m,[_,s(o)]),v,h,j]),y,s(l,null,{default:e(()=>[s(t,{title:"*"},{default:e(()=>[g]),_:1}),s(t,{title:"Lua 5.1"},{default:e(()=>[f]),_:1}),s(t,{title:"Lua 5.2"},{default:e(()=>[w]),_:1}),s(t,{title:"Lua 5.3"},{default:e(()=>[x]),_:1}),s(t,{title:"Lua 5.4"},{default:e(()=>[q]),_:1}),s(t,{title:"LuaJIT"},{default:e(()=>[b]),_:1})]),_:1}),J,a("p",null,[L,a("a",T,[C,s(o)]),I,a("a",G,[M,s(o)]),B]),E])}var A=u(r,[["render",N],["__file","jpms.html.vue"]]);export{A as default};
