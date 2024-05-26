6a3e1affa6bad849863327155ded21b391e7df4c
gudzpoz <gudzpoz@live.com>
2024-05-26 17:13:02 +0800

    Update jnigen version

--- scripts/jnigen-gradle.py
+++ scripts/jnigen-gradle.py
@@ -1,53 +1,70 @@
-buildscript {
-    repositories {
+#!/bin/python
+
+# for i in {1..4}; do python scripts/jnigen-gradle.py lua5${i} > lua5${i}/build.gradle; done
+
+import sys
+
+jnigen_version = "2.5.1"
+lua_version = sys.argv[1]
+assert lua_version in ["lua51", "lua52", "lua53", "lua54"]
+header_dirs = {
+    "lua51": "'lua51/src', 'lua51/etc'",
+    "lua52": "'lua52'",
+    "lua53": "'lua53'",
+    "lua54": "'lua54'",
+}
+
+script = f"""\
+buildscript {{
+    repositories {{
         mavenLocal()
         mavenCentral()
-    }
-    dependencies {
-        classpath 'com.badlogicgames.gdx:gdx-jnigen-gradle:2.5.1'
-    }
-}
+    }}
+    dependencies {{
+        classpath 'com.badlogicgames.gdx:gdx-jnigen-gradle:{jnigen_version}'
+    }}
+}}
 
-plugins {
+plugins {{
     id 'java'
     id 'java-library'
-}
+}}
 
-repositories {
+repositories {{
     mavenCentral()
-}
+}}
 
 group = rootProject.group
 version = rootProject.version
 
-configurations {
-    desktopNatives {
+configurations {{
+    desktopNatives {{
         canBeConsumed = true
         canBeResolved = false
-    }
+    }}
 
-    instrumentedJars {
+    instrumentedJars {{
         canBeConsumed = true
         canBeResolved = false
         extendsFrom api, implementation, runtimeOnly
-    }
-}
+    }}
+}}
 
-dependencies {
+dependencies {{
     api project(':luajava')
-}
+}}
 
 apply plugin: 'com.badlogicgames.gdx.gdx-jnigen'
 
-jnigen {
-    sharedLibName = 'lua52'
+jnigen {{
+    sharedLibName = '{lua_version}'
 
-    all {
-        headerDirs = ['../../jni/luajava', 'mod', 'lua52']
-        cppExcludes = ['lua52/**/*']
-        cExcludes = ['lua52/**/*']
+    all {{
+        headerDirs = ['../../jni/luajava', 'mod', {header_dirs[lua_version]}]
+        cppExcludes = ['{lua_version}/**/*']
+        cExcludes = ['{lua_version}/**/*']
         libraries = ' -lm '
-    }
+    }}
 
     add(Windows, x32)
     add(Windows, x64)
@@ -57,22 +74,22 @@ jnigen {
     add(Linux, x64)
     add(Linux, x32, ARM)
     add(Linux, x64, ARM)
-    each({ it.os == Linux }) {
+    each({{ it.os == Linux }}) {{
         String linuxFlags = ' -D_FORTIFY_SOURCE=0 -DLUA_USE_DLOPEN '
         cFlags += linuxFlags
         cppFlags += linuxFlags
-    }
+    }}
 
     add(MacOsX, x64)
     add(MacOsX, x64, ARM)
-    each({ it.os == MacOsX }) {
+    each({{ it.os == MacOsX }}) {{
         String macFlags = ' -DLUA_USE_DLOPEN '
         libraries = ''
         cFlags += macFlags
         cppFlags += macFlags
-    }
+    }}
 
-    add(Android) {
+    add(Android) {{
         String androidFlags = ' -D_FORTIFY_SOURCE=1 -DLUA_USE_DLOPEN '
         cFlags += androidFlags
         cppFlags += androidFlags
@@ -80,22 +97,25 @@ jnigen {
                 'APP_PLATFORM := android-21',
                 "APP_CFLAG :=$androidFlags",
         ]
-    }
+    }}
 
-    add(IOS) {
+    add(IOS) {{
         libraries = ''
-    }
-}
+    }}
+}}
 
-artifacts {
+artifacts {{
     instrumentedJars(jar)
     desktopNatives(jnigenJarNativesDesktop)
-}
+}}
 
-tasks.named('jar') {
-    manifest {
-        attributes('Automatic-Module-Name': 'party.iroiro.luajava.lua52')
-    }
-}
+tasks.named('jar') {{
+    manifest {{
+        attributes('Automatic-Module-Name': 'party.iroiro.luajava.{lua_version}')
+    }}
+}}
 
 tasks.jnigen.dependsOn(tasks.build)
+"""
+
+print(script, end="")
