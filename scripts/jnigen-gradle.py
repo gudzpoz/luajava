f8a8a431ea598a862c24bb2446ffb9d293a7a6da
gudzpoz <gudzpoz@live.com>
2025-12-30 18:52:17 +0800

    build: upgrade jnigen to v3

--- scripts/jnigen-gradle.py
+++ scripts/jnigen-gradle.py
@@ -5,14 +5,14 @@
 
 import sys
 
-jnigen_version = "2.5.2"
+jnigen_version = "3.1.1"
 lua_version = sys.argv[1]
 assert lua_version in ["lua51", "lua52", "lua53", "lua54"]
 header_dirs = {
-    "lua51": "'lua51/src', 'lua51/etc'",
-    "lua52": "'lua52'",
-    "lua53": "'lua53'",
-    "lua54": "'lua54'",
+    "lua51": "'jni/lua51/src', 'jni/lua51/etc'",
+    "lua52": "'jni/lua52'",
+    "lua53": "'jni/lua53'",
+    "lua54": "'jni/lua54'",
 }
 
 compat_flags = {
@@ -28,7 +28,7 @@ buildscript {{
         mavenCentral()
     }}
     dependencies {{
-        classpath 'com.badlogicgames.gdx:gdx-jnigen-gradle:{jnigen_version}'
+        classpath 'com.badlogicgames.jnigen:jnigen-gradle:{jnigen_version}'
     }}
 }}
 
@@ -61,47 +61,47 @@ dependencies {{
     api project(':luajava')
 }}
 
-apply plugin: 'com.badlogicgames.gdx.gdx-jnigen'
+apply plugin: 'com.badlogicgames.jnigen.jnigen-gradle'
 
 jnigen {{
     sharedLibName = '{lua_version}'
 
     all {{{'' if lua_version not in compat_flags else f'''
-        cFlags += ' {compat_flags[lua_version]}'
-        cppFlags += ' {compat_flags[lua_version]}\''''}
-        headerDirs = ['../../jni/luajava', 'mod', {header_dirs[lua_version]}]
-        cppExcludes = ['{lua_version}/**/*']
-        cExcludes = ['{lua_version}/**/*']
-        libraries = ' -lm '
+        cFlags += ['{compat_flags[lua_version]}']
+        cppFlags += ['{compat_flags[lua_version]}']'''}
+        headerDirs = ['../jni/luajava', 'jni/mod', {header_dirs[lua_version]}]
+        cppExcludes = ['jni/{lua_version}/**/*']
+        cExcludes = ['jni/{lua_version}/**/*']
+        libraries = ['-lm']
     }}
 
-    add(Windows, x32)
-    add(Windows, x64)
-    add(Windows, x64, ARM)
+    addWindows(x32, x86)
+    addWindows(x64, x86)
+    addWindows(x64, ARM)
 
-    add(Linux, x32)
-    add(Linux, x64)
-    add(Linux, x32, ARM)
-    add(Linux, x64, ARM)
+    addLinux(x32, x86)
+    addLinux(x64, x86)
+    addLinux(x32, ARM)
+    addLinux(x64, ARM)
     // TODO: Until we have a RISCV toolchain on Ubuntu.
-    // add(Linux, x64, RISCV)
+    // addLinux(x64, RISCV)
     each({{ it.os == Linux }}) {{
-        String linuxFlags = ' -D_FORTIFY_SOURCE=0 -DLUA_USE_DLOPEN '
+        String[] linuxFlags = ['-D_FORTIFY_SOURCE=0', '-DLUA_USE_DLOPEN']
         cFlags += linuxFlags
         cppFlags += linuxFlags
     }}
 
-    add(MacOsX, x64)
-    add(MacOsX, x64, ARM)
+    addMac(x64, x86)
+    addMac(x64, ARM)
     each({{ it.os == MacOsX }}) {{
-        String macFlags = ' -DLUA_USE_DLOPEN '
+        String[] macFlags = ['-DLUA_USE_DLOPEN']
         libraries = ''
         cFlags += macFlags
         cppFlags += macFlags
     }}
 
-    add(Android) {{
-        String androidFlags = ' -D_FORTIFY_SOURCE=1 -DLUA_USE_DLOPEN '
+    addAndroid {{
+        String[] androidFlags = ['-D_FORTIFY_SOURCE=1', '-DLUA_USE_DLOPEN']
         cFlags += androidFlags
         cppFlags += androidFlags
         androidApplicationMk = [
@@ -112,17 +112,29 @@ jnigen {{
 
     robovm {{
         forceLinkClasses "java.lang.Class", "java.lang.Throwable", "party.iroiro.luajava.JuaAPI"
-    }}
-    add(IOS, x64) {{
-        libraries = ''
         xcframeworkBundleIdentifier = "party.iroiro.luajava.{lua_version}"
         minIOSVersion = "11.0"
     }}
+    addIOS()
 }}
 
 artifacts {{
+    artifacts {{
+        [
+                tasks.jnigenPackageAllAndroid,
+                tasks.jnigenPackageAllIOS,
+                tasks.jnigenPackageAllDesktop,
+        ].forEach {{ packageTask ->
+            packageTask.outputs.files.forEach {{ file ->
+                archives(file) {{
+                    builtBy(packageTask)
+                }}
+            }}
+        }}
+    }}
+
     instrumentedJars(jar)
-    desktopNatives(jnigenJarNativesDesktop)
+    desktopNatives(jnigenPackageAllDesktop.outputs.files.files)
 }}
 
 tasks.named('jar') {{
