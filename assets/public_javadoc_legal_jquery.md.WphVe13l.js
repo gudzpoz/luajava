023ce3f78a308e2924b7f608229aa87afec22565
gudzpoz <GuDzpoz@live.com>
2026-01-03 12:43:13 +0000

    Deploying to gh-pages from @ gudzpoz/luajava@c05364e6ca9bf1a7da0a958ac72d6672e8f33df6 🚀

---
+++ assets/public_javadoc_legal_jquery.md.WphVe13l.js
@@ -0,0 +1,21 @@
+import{_ as n,c as a,o as e,a7 as p}from"./chunks/framework.FXXHEqG5.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"public/javadoc/legal/jquery.md","filePath":"public/javadoc/legal/jquery.md","lastUpdated":null}'),l={name:"public/javadoc/legal/jquery.md"};function i(r,s,t,o,c,u){return e(),a("div",{"data-pagefind-body":!0,"data-pagefind-meta":"date:1767444170916"},[...s[0]||(s[0]=[p(`<h2 id="jquery-v3-7-1" tabindex="-1">jQuery v3.7.1 <a class="header-anchor" href="#jquery-v3-7-1" aria-label="Permalink to &quot;jQuery v3.7.1&quot;">​</a></h2><h3 id="jquery-license" tabindex="-1">jQuery License <a class="header-anchor" href="#jquery-license" aria-label="Permalink to &quot;jQuery License&quot;">​</a></h3><div class="language- vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>jQuery v 3.7.1</span></span>
+<span class="line"><span>Copyright OpenJS Foundation and other contributors, https://openjsf.org/</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Permission is hereby granted, free of charge, to any person obtaining</span></span>
+<span class="line"><span>a copy of this software and associated documentation files (the</span></span>
+<span class="line"><span>&quot;Software&quot;), to deal in the Software without restriction, including</span></span>
+<span class="line"><span>without limitation the rights to use, copy, modify, merge, publish,</span></span>
+<span class="line"><span>distribute, sublicense, and/or sell copies of the Software, and to</span></span>
+<span class="line"><span>permit persons to whom the Software is furnished to do so, subject to</span></span>
+<span class="line"><span>the following conditions:</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>The above copyright notice and this permission notice shall be</span></span>
+<span class="line"><span>included in all copies or substantial portions of the Software.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND,</span></span>
+<span class="line"><span>EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF</span></span>
+<span class="line"><span>MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND</span></span>
+<span class="line"><span>NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE</span></span>
+<span class="line"><span>LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION</span></span>
+<span class="line"><span>OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION</span></span>
+<span class="line"><span>WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br></div></div>`,3)])])}const m=n(l,[["render",i]]);export{d as __pageData,m as default};
---
+++ assets/public_javadoc_legal_jqueryUI.md.BwHO4f9B.js
@@ -0,0 +1,43 @@
+import{_ as n,c as a,o as e,a7 as p}from"./chunks/framework.FXXHEqG5.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"public/javadoc/legal/jqueryUI.md","filePath":"public/javadoc/legal/jqueryUI.md","lastUpdated":null}'),l={name:"public/javadoc/legal/jqueryUI.md"};function i(r,s,c,t,o,b){return e(),a("div",{"data-pagefind-body":!0,"data-pagefind-meta":"date:1767444170917"},[...s[0]||(s[0]=[p(`<h2 id="jquery-ui-v1-14-1" tabindex="-1">jQuery UI v1.14.1 <a class="header-anchor" href="#jquery-ui-v1-14-1" aria-label="Permalink to &quot;jQuery UI v1.14.1&quot;">​</a></h2><h3 id="jquery-ui-license" tabindex="-1">jQuery UI License <a class="header-anchor" href="#jquery-ui-license" aria-label="Permalink to &quot;jQuery UI License&quot;">​</a></h3><div class="language- vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Copyright OpenJS Foundation and other contributors, https://openjsf.org/</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>This software consists of voluntary contributions made by many</span></span>
+<span class="line"><span>individuals. For exact contribution history, see the revision history</span></span>
+<span class="line"><span>available at https://github.com/jquery/jquery-ui</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>The following license applies to all parts of this software except as</span></span>
+<span class="line"><span>documented below:</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>====</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Permission is hereby granted, free of charge, to any person obtaining</span></span>
+<span class="line"><span>a copy of this software and associated documentation files (the</span></span>
+<span class="line"><span>&quot;Software&quot;), to deal in the Software without restriction, including</span></span>
+<span class="line"><span>without limitation the rights to use, copy, modify, merge, publish,</span></span>
+<span class="line"><span>distribute, sublicense, and/or sell copies of the Software, and to</span></span>
+<span class="line"><span>permit persons to whom the Software is furnished to do so, subject to</span></span>
+<span class="line"><span>the following conditions:</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>The above copyright notice and this permission notice shall be</span></span>
+<span class="line"><span>included in all copies or substantial portions of the Software.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND,</span></span>
+<span class="line"><span>EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF</span></span>
+<span class="line"><span>MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND</span></span>
+<span class="line"><span>NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE</span></span>
+<span class="line"><span>LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION</span></span>
+<span class="line"><span>OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION</span></span>
+<span class="line"><span>WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>====</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Copyright and related rights for sample code are waived via CC0. Sample</span></span>
+<span class="line"><span>code is defined as all source code contained within the demos directory.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>CC0: http://creativecommons.org/publicdomain/zero/1.0/</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>====</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>All files located in the node_modules and external directories are</span></span>
+<span class="line"><span>externally maintained libraries used by this software which have their</span></span>
+<span class="line"><span>own licenses; we recommend you read them, as their terms may differ from</span></span>
+<span class="line"><span>the terms above.</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br><span class="line-number">24</span><br><span class="line-number">25</span><br><span class="line-number">26</span><br><span class="line-number">27</span><br><span class="line-number">28</span><br><span class="line-number">29</span><br><span class="line-number">30</span><br><span class="line-number">31</span><br><span class="line-number">32</span><br><span class="line-number">33</span><br><span class="line-number">34</span><br><span class="line-number">35</span><br><span class="line-number">36</span><br><span class="line-number">37</span><br><span class="line-number">38</span><br><span class="line-number">39</span><br><span class="line-number">40</span><br><span class="line-number">41</span><br><span class="line-number">42</span><br><span class="line-number">43</span><br></div></div>`,3)])])}const m=n(l,[["render",i]]);export{d as __pageData,m as default};
